a774800ab29f5913b895a92f6629ee6f30dca548
ChammaMalak <155775919+ChammaMalak@users.noreply.github.com>
2024-12-06 17:14:07 +0100

    Update RendezVous.java

--- RendezVous.java
+++ RendezVous.java
@@ -1,5 +1,12 @@
 package projetpoo;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class RendezVous{
-  
+  private LocalDate date;
+	private LocalTime heure;
+	private Patient patient;
+	private boolean medecinDisponible;
+
 }
